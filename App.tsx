5a9693b38d6bb097aa1e30eaf26c8e9907892865
Bagus Aditama <bagusaditama.pp@gmail.com>
2024-02-26 12:53:27 +0700

    feat: fetch json from API feat: fix styles

--- App.tsx
+++ App.tsx
@@ -1,90 +1,126 @@
-import * as React from 'react';
-import {
-  FlatList,
-  // Alert,
-  // Animated,
-  StyleSheet,
-  // TouchableOpacity,
-  Text,
-  View,
-} from 'react-native';
-// import {BottomNavigation} from 'reactjs-bottom-navigation';
-// import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-// import Icons from 'react-native-vector-icons/FontAwesome5';
-// import {CurvedBottomBar} from 'react-native-curved-bottom-bar';
-// import Ionicons from 'react-native-vector-icons/Ionicons';
+import React, {Component} from 'react';
+import {FlatList, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {faHouse} from '@fortawesome/free-solid-svg-icons/faHouse';
+import {faUser} from '@fortawesome/free-solid-svg-icons/faUser';
 
 const styles = StyleSheet.create({
-  item: {
+  listTile: {
     padding: 10,
-    fontSize: 18,
-    height: 44,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
-  flatListContent: {
-    flexGrow: 1, // Allow FlatList to grow to fill its parent
+  listTileTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'black',
   },
-  flatList: {
-    width: '100%', // Fill the parent's width
+  listTileSubtitle: {
+    color: 'black',
   },
 });
 
+interface Item {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface State {
+  jsonData: Item[];
+}
+
 const Tab = createBottomTabNavigator();
-function HomeScreen() {
+
+function ProfileScreen() {
   return (
-    <View
-      // eslint-disable-next-line react-native/no-inline-styles
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <FlatList
-        data={[
-          {key: 'Devin'},
-          {key: 'Dan'},
-          {key: 'Dominic'},
-          {key: 'Jackson'},
-          {key: 'James'},
-          {key: 'Joel'},
-          {key: 'John'},
-          {key: 'Jillian'},
-          {key: 'Jimmy'},
-          {key: 'Julie'},
-          {key: 'Devina'},
-          {key: 'Dana'},
-          {key: 'Dominica'},
-          {key: 'Jacksona'},
-          {key: 'Jamesa'},
-          {key: 'Joela'},
-          {key: 'Johna'},
-          {key: 'Jilliana'},
-          {key: 'Jimmya'},
-          {key: 'Juliea'},
-        ]}
-        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-        contentContainerStyle={styles.flatListContent}
-        style={styles.flatList}
-      />
+    // eslint-disable-next-line react-native/no-inline-styles
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 18, color: 'black'}}>Settings</Text>
     </View>
   );
 }
 
-function SettingsScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Settings</Text>
-    </View>
-  );
+class ListScreen extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      jsonData: [],
+    };
+  }
+
+  componentDidMount() {
+    // Fetch JSON data from the API
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    try {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/posts',
+      );
+      const jsonData: Item[] = await response.json();
+      this.setState({jsonData});
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  renderItem = ({item}: {item: Item}) => {
+    return (
+      <TouchableOpacity onPress={() => this.handleItemClick(item)}>
+        <View style={styles.listTile}>
+          <Text style={styles.listTileTitle}>{item.title}</Text>
+          <Text style={styles.listTileSubtitle}>{item.body}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  handleItemClick = (item: Item) => {
+    console.log('Item Clicked:', item);
+  };
+
+  render() {
+    return (
+      // eslint-disable-next-line react-native/no-inline-styles
+      <View style={{flex: 1}}>
+        <FlatList
+          data={this.state.jsonData}
+          renderItem={this.renderItem}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+    );
+  }
 }
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="List" component={HomeScreen} />
-        <Tab.Screen name="Profile" component={SettingsScreen} />
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({size, color}) => {
+            let iconName;
+
+            if (route.name === 'List') {
+              iconName = faHouse;
+            } else if (route.name === 'Profile') {
+              iconName = faUser;
+            }
+
+            return (
+              <FontAwesomeIcon icon={iconName} size={size} color={color} />
+            );
+          },
+        })}>
+        <Tab.Screen name="List" component={ListScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
